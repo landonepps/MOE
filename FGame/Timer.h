125e728886c6b3e22a7e943d8f959cb96a147fde
JesseTime <jesselclary@gmail.com>
2014-02-07 18:49:53 -0600

    Implemented Timer

---
+++ FGame/Timer.h
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ * Authors:    Jesse Clary
+ *             Landon Epps
+ *             Andrew Kliphon
+ *             Jeremy Stark
+ * Assignment: MOE (Most Optimistic Engine)
+ * Class:      4342, Spring 2014
+ * Date:      7-Feb-2014
+ *
+ * Timer class
+*******************************************************************************/
+
+#ifndef TIMER_H_
+#define TIMER_H_
+
+#include <SDL2\SDL.h>
+#include <string>
+#include "SDL_ttf.h"
+#include "HUDelement.h"
+
+using namespace std;
+
+class Timer : public HUDelement{
+    private:
+        int xPosition;
+        int yPosition;
+        int seconds;
+        TTF_Font *font;
+        SDL_Color color;
+    public:
+        Timer(SDL_Color cl, const string &fontName = NULL,int fontSize = 12, int xPos = 0, int yPos = 0, int sec = 0, 
+            int id = NULL) : HUDelement(id)
+        {
+            xPosition = xPos;
+            yPosition = yPos;
+            seconds = sec;
+            color = cl;
+            font = TTF_OpenFont(fontName.c_str(), fontSize);
+        }
+        void draw(SDL_Surface*);
+};
+
+#endif
---
+++ FGame/src/Timer.cpp
@@ -0,0 +1,42 @@
+/*******************************************************************************
+ * Authors:    Jesse Clary
+ *             Landon Epps
+ *             Andrew Kliphon
+ *             Jeremy Stark
+ * Assignment: MOE (Most Optimistic Engine)
+ * Class:      4342, Spring 2014
+ * Date:      7-Feb-2014
+ *
+ * Timer class
+*******************************************************************************/
+
+#include "Timer.h"
+
+Timer::~Timer(){
+    SDL_DestroyTexture(texture);
+}
+
+void Timer::draw(SDL_Renderer* renderer){
+    int currentTime = seconds - clock.cyclesToSeconds(clock.getTimeCycles());
+    stringstream message;
+    message << currentTime;
+
+    SDL_Surface *surf = TTF_RenderText_Blended(font, message.str().c_str(), color);
+    if (surf == nullptr){
+		TTF_CloseFont(font);
+		return;
+	}
+
+    texture = SDL_CreateTextureFromSurface(renderer, surf);
+
+    SDL_Rect rect;
+    rect.x = xPosition;
+    rect.y = yPosition;
+
+    SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h);
+
+    SDL_RenderCopy(renderer, texture, NULL, &rect);
+
+    SDL_FreeSurface(surf);
+    TTF_CloseFont(font);
+}

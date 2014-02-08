a594570f59475441161bb17e19fdd6bd6ac634fc
JesseTime <jesselclary@gmail.com>
2014-02-07 21:26:07 -0600

    Timer now renders (needs to decrement time correctly)

--- FGame/src/Timer.cpp
+++ FGame/src/Timer.cpp
@@ -13,14 +13,35 @@
 #include "Timer.h"
 #include <iostream>
 
+Timer::Timer() : HUDelement(){}
+
+Timer::Timer(int id) : HUDelement(id){}
+
+void Timer::setup(SDL_Color cl, const string &fontFile,int fontSize, 
+            int xPos, int yPos, int sec, int id)
+{
+    clock.init();
+    xPosition = xPos;
+    yPosition = yPos;
+    seconds = sec;
+    font = TTF_OpenFont(fontFile.c_str(), fontSize);
+    if (font == NULL) {
+        cerr << "failed to load font";
+    }
+    color = cl;
+    texture = NULL;
+}
+
 Timer::~Timer(){
     SDL_DestroyTexture(texture);
+    TTF_CloseFont(font);
 }
 
 void Timer::draw(SDL_Renderer* renderer){
+       
     int currentTime = seconds - clock.cyclesToSeconds(clock.getTimeCycles());
     stringstream message;
-    message << "I loke p[ee"; // << currentTime;
+    message << currentTime;
 
     SDL_Surface *surf = TTF_RenderText_Blended(font, message.str().c_str(), color);
     if (surf == nullptr){
@@ -43,5 +64,4 @@ void Timer::draw(SDL_Renderer* renderer){
     SDL_RenderCopy(renderer, texture, NULL, &rect);
 
     SDL_FreeSurface(surf);
-    TTF_CloseFont(font);
 }

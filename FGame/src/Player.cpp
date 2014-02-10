01cababf712704760f879b62afdb29ffb81ef450
JeremyStark <Jeremy_Stark@baylor.edu>
2014-02-09 21:34:34 -0600

    Update Player.cpp

--- FGame/src/Player.cpp
+++ FGame/src/Player.cpp
@@ -1,6 +1,6 @@
 #include "Player.h"
  
-Player::Player(int health = 100) {
+Player::Player() {
     surfEntity = NULL;
  
     xLoc = 0;
@@ -38,13 +38,52 @@ Player::Player(int health = 100) {
 	currentHealth = 100;
 	maxHealth = 100;
 }
+
+Player::Player(int health) {
+    surfEntity = NULL;
+ 
+    xLoc = 0;
+    yLoc = 0;
+ 
+    width  = 0;
+    height = 0;
+ 
+    moveLeft  = false;
+    moveRight = false;
+
+    entType = ENTITY_TYPE_PLAYER;
+ 
+    isDead = false;
+    flags = ENTITY_FLAG_GRAVITY;
+ 
+    xSpd = 0;
+    ySpd= 0;
+ 
+    xAccel = 0;
+    yAccel = 0;
+ 
+    xSpdMax = 5;
+    ySpdMax = 5;
+ 
+    frameCol = 0;
+    frameRow = 0;
+ 
+    xCollision = 0;
+    yCollision = 0;
+ 
+    colWidth  = 0;
+    colHeight = 0;
+
+	currentHealth = health;
+	maxHealth = health;
+}
  
 bool Player::OnLoad(char* File, int Width, int Height, int MaxFrames) {
     return Entity::OnLoad(File, Width, Height, MaxFrames);
 }
  
-void Player::OnLoop() {
-    Entity::OnLoop();
+void Player::OnLoop(Clock* clock) {
+    Entity::OnLoop(clock);
 }
  
 void Player::OnRender(SDL_Surface* surfDisplay) {
@@ -55,8 +94,8 @@ void Player::OnCleanup() {
     Entity::OnCleanup();
 }
  
-void Player::OnAnimate() {
-    Entity::OnAnimate();
+void Player::OnAnimate(Clock *clock) {
+    Entity::OnAnimate(clock);
 }
  
 void Player::OnCollision(Entity* entity) {

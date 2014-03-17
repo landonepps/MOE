4dd97d93e3dfc0c0d947bdede47e0dbd7b4b7104
AndrewKliphon <Andrew_Kliphon@baylor.edu>
2014-03-17 18:08:03 -0500

    Added comments.

--- FGame/src/SceneGraphNode.h
+++ FGame/src/SceneGraphNode.h
@@ -18,25 +18,99 @@
 class SceneGraphNode
 {
     public:
+        /***********************************************************************
+         * SceneGraphNode: Constructor.
+         *
+         * mesh:      The mesh to be drawn.
+        ***********************************************************************/
         SceneGraphNode(Mesh *mesh);
+
+        /***********************************************************************
+         * SceneGraphNode: Destructor.
+        ***********************************************************************/
         virtual ~SceneGraphNode();
 
+
+
+        /***********************************************************************
+         * setTransformation: Sets this node's transformation matrix.
+         *
+         * matrix:    The transformation matrix to be applied.
+         *
+         * return:    void
+        ***********************************************************************/
         void setTransformation(glm::mat4 matrix);
+
+        /***********************************************************************
+         * getTransformation: Returns this node's transformation matrix.
+         *
+         * return:    mat4
+        ***********************************************************************/
         glm::mat4 getTransformation();
+
+        /***********************************************************************
+         * getWorldTransformation: Returns this node's world transformation
+         *            matrix.
+         *
+         * return:    mat4
+        ***********************************************************************/
         glm::mat4 getWorldTransformation();
 
+        /***********************************************************************
+         * getMesh:   Returns a pointer to this node's mesh.
+         *
+         * return:    Mesh pointer
+        ***********************************************************************/
         Mesh *getMesh();
+
+        /***********************************************************************
+         * setMesh:   Sets this node's mesh.
+         *
+         * mesh:      The mesh to be drawn.
+         *
+         * return:    void
+        ***********************************************************************/
         void setMesh(Mesh *mesh);
 
-        void insertChild(SceneGraphNode *node);
+
+
+        /***********************************************************************
+         * addChild:  Adds a child node to this parent (if no parent, then
+         *            parent will be NULL).
+         *
+         * node:      The child node.
+         *
+         * return:    void
+        ***********************************************************************/
+        void addChild(SceneGraphNode *node);
+
+        /***********************************************************************
+         * update:    Updates all nodes applying parent transformations to child
+         *            nodes.
+         *
+         * return:    void
+        ***********************************************************************/
         virtual void update();
+
+        /***********************************************************************
+         * draw:      Draws all meshs in the scene graph.
+         *
+         * return:    void
+        ***********************************************************************/
         void draw();
 
     protected:
+        /** This node's parent. **/
         SceneGraphNode *parent;
+        /** This node's mesh. **/
         Mesh *mesh;
+        /** This node's local transformation. Will be a global transformation
+         *  for its children.**/
         glm::mat4 transformation;
+        /** This node's global transformation (to be applied before local
+         *  transformation). **/
         glm::mat4 worldTransformation;
+        /** This node's children. **/
         vector<SceneGraphNode*> children;
 };
 
